9c128de771f89e5f96ed3a3dc9d8b935f48520b6
League Student <league@iMac-8.attlocal.net>
2018-10-13 12:27:10 -0700

    i did the checkpoint today 10/13/18
    for this module, i did the else if,loops,modulo etc. and more

---
+++ src/checkpointmodulo3.java
@@ -0,0 +1,22 @@
+
+public class checkpointmodulo3 {
+public static void main(String[] args) {
+
+
+
+	for (int i=1; i<9; i++){
+		if (i==8){
+		System.out.print("more");
+		}
+		else if (i==4){
+		System.out.println(i);
+		}
+		else {
+		System.out.print(i+ " potato, ");
+		}
+		}
+}
+	
+}
+
+
